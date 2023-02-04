c9dc70f23b834decba87de368080ab9836ca670f
shinjons99 <shinjons99@gmail.com>
2023-02-04 23:47:04 +0600

    Again parameter after sum

--- parameter.js
+++ parameter.js
@@ -10,4 +10,16 @@ function bringSingara(money){
     console.log('Ai nen singara');
 }
 let taka = 300;
-bringSingara(taka);
+// bringSingara(taka);
+
+function add(num1, num2){
+    console.log('Going to add:', num1, num2);
+}
+// add(124, 96);
+
+function sum(a, b, c, d, e){
+    let sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(15, 98, 56, 20, 67);
