cdc6760cfef801935c760b5f19c62f72bd6b0252
shinjons99 <shinjons99@gmail.com>
2023-02-04 23:41:29 +0600

    Again parameter work

--- parameter.js
+++ parameter.js
@@ -1,6 +1,13 @@
-function bringSingara(money){
-    console.log('Mama singara den');
-    console.log(money);
-}
+// function bringSingara(money){
+//     console.log('Mama singara den');
+//     console.log(money);
+// }
 
-bringSingara(250);
+// bringSingara(250);
+
+function bringSingara(money){
+    console.log('Eto taka disen', money);
+    console.log('Ai nen singara');
+}
+let taka = 300;
+bringSingara(taka);
