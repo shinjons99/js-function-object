64a76780a3d000f58d2850241d35ab3d56288b34
shinjons99 <shinjons99@gmail.com>
2023-02-04 23:27:09 +0600

    first commit function

---
+++ function.js
@@ -0,0 +1,14 @@
+//function Declaration
+function startFan(){
+    console.log('Stand up');
+    console.log('Walk towards the switch');
+    console.log('Press the switch');
+}
+
+//call the function
+startFan();
+console.log('Waking up in the morning');
+startFan();
+console.log('Eating Lunch');
+startFan();
+console.log('Watching js Tutorial');
